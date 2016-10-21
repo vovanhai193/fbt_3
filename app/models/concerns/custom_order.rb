9158a9fa399181bf0956506a36b0054fe06673e3
Quynh Thai <thaiquynh.inbox@gmail.com>
2016-10-21 14:19:57 +0700

    Show edit delete review

--- app/models/concerns/custom_order.rb
+++ app/models/concerns/custom_order.rb
@@ -2,7 +2,7 @@ module CustomOrder
   extend ActiveSupport::Concern
 
   included do
-    scope :latest, ->{where created_at: :desc}
-    scope :sort_by_title, ->{where title: :asc}
+    scope :latest, ->{order created_at: :desc}
+    scope :sort_by_title, ->{order title: :asc}
   end
 end
